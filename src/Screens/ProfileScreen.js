0220282448539108fd741534a97ea8db9080d802
Christian-cmd-ui <danbel418@gmail.com>
2023-05-26 22:37:57 +0100

    fourth commit

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,12 +1,26 @@
-import { Box, Center} from 'native-base';
+import { Box, Center, Heading, Image, Text} from 'native-base';
 import React from 'react'
+import Colors from '../Colors'
 
 
 function ProfileScreen() {
   return (
-    <Box>
-        <Center bg={Colors.gray}>ProfileScreen</Center>
-    </Box>
+    
+        <Center bg={Colors.main} pt={10} pb={6} >
+            <Image 
+            source={{uri:"https://res.cloudinary.com/dpflxilm6/image/upload/v1685136398/Dalii/user_1_ntkewy.png"}} 
+            alt="profile" 
+            w={24}
+            h={24}
+            resizeMode="cover" />
+            <Heading bold fontSize={15} isTruncated my={2} color={Colors.black}>
+              Utilisateur 1
+            </Heading>
+            <Text italic fontSize={10} color={Colors.black}>
+              Cree le 24 Mai 2023
+            </Text>
+        </Center>
+        
   )
 }
 
